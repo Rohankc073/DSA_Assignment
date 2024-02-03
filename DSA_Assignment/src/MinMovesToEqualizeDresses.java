668ec54b579ffeb76d77e2cc0bd486f6143db202
Rohankc073 <125729603+Rohankc073@users.noreply.github.com>
2024-02-03 18:48:11 +0545

    Initial Commit

---
+++ DSA_Assignment/src/MinMovesToEqualizeDresses.java
@@ -0,0 +1,34 @@
+public class MinMovesToEqualizeDresses {
+
+    public static int minMovesToEqualize(int[] sewingMachines) {
+        int totalDresses = 0;
+        int numMachines = sewingMachines.length;
+
+        for (int dresses : sewingMachines) {
+            totalDresses += dresses;
+        }
+
+        if (totalDresses % numMachines != 0) {
+            return -1;
+        }
+
+        int targetDresses = totalDresses / numMachines;
+        int moves = 0;
+        int accumulatedDifference = 0;
+
+        for (int dresses : sewingMachines) {
+            int difference = dresses - targetDresses;
+            accumulatedDifference += difference;
+            moves = Math.max(moves, Math.abs(accumulatedDifference));
+        }
+
+        return moves;
+    }
+
+    public static void main(String[] args) {
+        int[] sewingMachines = {2, 1, 3, 0, 2};
+        int result = minMovesToEqualize(sewingMachines);
+
+        System.out.println("Minimum moves required: " + result);
+    }
+}
---
+++ DSA_Assignment/src/SecretSharing.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class SecretSharing {
+    public static List<Integer> getIndividuals(int n, int[][] intervals, int firstPerson) {
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> knownSecrets = new HashSet<>();
+        knownSecrets.add(firstPerson);
+
+        for (int[] interval : intervals) {
+            int start = interval[0];
+            int end = interval[1];
+
+            Set<Integer> newIndividuals = new HashSet<>();
+
+            for (int i = start; i <= end; i++) {
+                if (knownSecrets.contains(i)) {
+                    // Add individuals who receive the secret during this interval
+                    for (int j = 0; j < n; j++) {
+                        if (!knownSecrets.contains(j)) {
+                            newIndividuals.add(j);
+                        }
+                    }
+                }
+            }
+
+            knownSecrets.addAll(newIndividuals);
+        }
+
+        result.addAll(knownSecrets);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[][] intervals = {{0, 2}, {1, 3}, {2, 4}};
+        int firstPerson = 0;
+
+        List<Integer> result = getIndividuals(n, intervals, firstPerson);
+
+        System.out.println("Individuals who will eventually know the secret: " + result);
+    }
+}
